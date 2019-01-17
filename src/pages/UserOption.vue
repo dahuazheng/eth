d14757ec3ccbc35c44d22b0b74aaaf52786d317c
vera <wanghemeng@viinet.com>
2019-01-17 23:08:06 +0800

    feat: sign out

--- src/pages/UserOption.vue
+++ src/pages/UserOption.vue
@@ -1,56 +1,83 @@
 <template>
     <div id="user-center">
-        <div class="user-center__banner">
-            <div class="icon-group">
-                <img class="logo" src="../assets/images/icon_eth_download.png" alt=""> <span>x</span> <img
+        <div>
+            <div class="user-center__banner">
+                <div class="icon-group">
+                    <img class="logo" src="../assets/images/icon_eth_download.png" alt=""> <span>x</span> <img
                     src="../assets/images/icon_eth_star.png" alt="">
+                </div>
+                <p>我的</p>
             </div>
-            <p>我的</p>
+
+            <div class="user-center__options">
+                <ul class="options">
+                    <!--<li>
+                        <router-link to="/select-lang">
+                            <span class='check-option__language'> 选择语言</span>
+                            <span class="lang-tip">简体中文</span>
+                            <span class='check-arrow'></span>
+                        </router-link>
+                    </li>-->
+                    <li>
+                        <router-link to="/game-intro">
+                            <span class="check-option__game">游戏说明</span>
+                            <span class='check-arrow'></span>
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link to="/common-problem">
+                            <span class="check-option__help">常见问题</span> <span class='check-arrow'/>
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link to="/contact-us">
+                            <span class="check-option__contact"> 联系我们</span> <span class='check-arrow'/>
+                        </router-link>
+                    </li>
+                    <li>
+                        <a @click="() => { this.isSignOut = true }" >
+                            <span class="check-option__out"> 退出登录</span>
+                            <span class='check-arrow'/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <EthFooter action="userOption"/>
         </div>
 
-        <div class="user-center__options">
-            <ul class="options">
-                <!--<li>
-                    <router-link to="/select-lang">
-                        <span class='check-option__language'> 选择语言</span>
-                        <span class="lang-tip">简体中文</span>
-                        <span class='check-arrow'></span>
-                    </router-link>
-                </li>-->
-                <li>
-                    <router-link to="/game-intro">
-                        <span class="check-option__game">游戏说明</span>
-                        <span class='check-arrow'></span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link to="/common-problem">
-                        <span class="check-option__help">常见问题</span> <span class='check-arrow'/>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link to="/contact-us">
-                        <span class="check-option__contact"> 联系我们</span> <span class='check-arrow'/>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link to="/login">
-                        <span class="check-option__out"> 退出登录</span>
-                        <span class='check-arrow'/>
-                    </router-link>
-                </li>
-            </ul>
+        <div :class="['dialog-shadow', !isSignOut && 'dialog-hidden'  ]">
+            <div class="dialog-container">
+                <div class="dialog-body">
+                    <span></span>
+                    确定退出当前帐号吗？
+                </div>
+                <div class="dialog-footer">
+                    <span @click="() => { this.isSignOut = false }">取消</span>
+                    <span @click="signOut">确定</span>
+                </div>
+            </div>
         </div>
-
-        <EthFooter action="userOption"/>
     </div>
 </template>
 <script>
   import EthFooter from '@/components/EthFooter.vue'
+  import { removeCookie } from '@/api/utils'
 
   export default {
     name: 'userOption',
     components: {EthFooter},
+    data() {
+        return {
+            isSignOut: false
+        }
+    },
+    methods: {
+        signOut() {
+            removeCookie('token')
+            this.$router.push('/')
+        }
+    }
   }
 </script>
 
@@ -58,144 +85,210 @@
     @import "../assets/styles/mixin";
 
     #user-center {
-        .user-center__banner {
-            position: relative;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-content: center;
-            @include px2rem('height', 237);
-            text-align: center;
-            background-image: url('../assets/images/bg_me_title.png');
-            background-size: 100% 100%;
-
-            .icon-group {
+        > div {
+            .user-center__banner {
                 position: relative;
                 display: flex;
-                flex-direction: row;
+                flex-direction: column;
                 justify-content: center;
-                align-items: center;
+                align-content: center;
+                @include px2rem('height', 237);
+                text-align: center;
+                background-image: url('../assets/images/bg_me_title.png');
+                background-size: 100% 100%;
 
-                img {
-                    @include px2rem('height', 46);
-                    @include px2rem('line-height', 52);
+                .icon-group {
+                    position: relative;
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: center;
+                    align-items: center;
 
-                    &.logo {
-                        @include px2rem('height', 64);
-                        @include px2rem('line-height', 64);
+                    img {
+                        @include px2rem('height', 46);
+                        @include px2rem('line-height', 52);
+
+                        &.logo {
+                            @include px2rem('height', 64);
+                            @include px2rem('line-height', 64);
+                        }
+                    }
+
+                    span {
+                        @include px2rem('height', 20);
+                        @include px2rem('line-height', 20);
+                        @include fontSize($font-large);
+                        position: absolute;
+                        top: 0;
+                        right: 0;
+                        bottom: 0;
+                        left: 6px;
+                        margin: auto;
+                        display: inline-block;
+                        color: #ffe500;
                     }
                 }
 
-                span {
-                    @include px2rem('height', 20);
-                    @include px2rem('line-height', 20);
-                    @include fontSize($font-large);
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                    bottom: 0;
-                    left: 6px;
-                    margin: auto;
-                    display: inline-block;
-                    color: #ffe500;
+                p {
+                    color: #fed67c;
+                    letter-spacing: 1px;
+                    font-size: $font-large;
+                    margin-top: 0;
                 }
             }
 
-            p {
-                color: #fed67c;
-                letter-spacing: 1px;
-                font-size: $font-large;
-                margin-top: 0;
+            .user-center__options {
+                padding-bottom: 36px;
+                padding-left: 36px;
+                padding-right: 36px;
+                padding-top: 10px;
+
+                ul.options {
+                    @include clearUl;
+                    display: flex;
+                    flex-direction: column;
+
+                    li > a {
+                        position: relative;
+                        display: flex;
+                        justify-content: space-between;
+                        align-items: center;
+                        padding: 20px 0;
+                        font-size: $font-little + 1;
+                        text-decoration: none;
+
+                        span {
+                            display: inline-flex;
+                            align-items: center;
+                            color: #5b5b5b;
+
+                            &.check-option__language::before {
+                                content: '';
+                                display: inline-block;
+                                margin-right: 10px;
+                                @include px2rem('width', 30);
+                                @include px2rem('height', 30);
+                                @include background-image();
+                                background-image: url('../assets/images/icon_language.png');
+                            }
+
+                            &.check-option__game::before {
+                                content: '';
+                                display: inline-block;
+                                margin-right: 10px;
+                                @include px2rem('width', 30);
+                                @include px2rem('height', 30);
+                                @include background-image();
+                                background-image: url('../assets/images/icon_game_des.png');
+                            }
+
+                            &.check-option__help::before {
+                                content: '';
+                                display: inline-block;
+                                margin-right: 10px;
+                                @include px2rem('width', 30);
+                                @include px2rem('height', 30);
+                                @include background-image();
+                                background-image: url('../assets/images/icon_helper.png');
+                            }
+
+                            &.check-option__contact::before {
+                                content: '';
+                                display: inline-block;
+                                margin-right: 10px;
+                                @include px2rem('width', 30);
+                                @include px2rem('height', 30);
+                                @include background-image();
+                                background-image: url('../assets/images/icon_commite_us.png');
+                            }
+
+                            &.check-option__out::before {
+                                content: '';
+                                display: inline-block;
+                                margin-right: 10px;
+                                @include px2rem('width', 30);
+                                @include px2rem('height', 30);
+                                @include background-image();
+                                background-image: url('../assets/images/icon_sign_out.png');
+                            }
+
+                            &.check-arrow {
+                                @include px2rem('width', 11);
+                                @include px2rem('height', 11);
+                                background-image: url('../assets/images/icon_push_right.png');
+                                @include background-image()
+                            }
+
+                            &.lang-tip {
+                                position: absolute;
+                                right: 30px;
+                                font-size: $font-little-s;
+                                color: #c4c2c2;
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        .user-center__options {
-            padding-bottom: 36px;
-            padding-left: 36px;
-            padding-right: 36px;
-            padding-top: 10px;
+        .dialog-shadow {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: rgba(0, 0, 0, 0.7);
 
-            ul.options {
-                @include clearUl;
+            &.dialog-hidden {
+                display: none;
+            }
+
+            .dialog-container {
                 display: flex;
                 flex-direction: column;
+                @include px2rem('width', 310);
+                @include px2rem('font-size', 15);
+                background-color: $clear-color;
+                border-radius: 8px;
 
-                li > a {
-                    position: relative;
+                .dialog-body {
                     display: flex;
-                    justify-content: space-between;
                     align-items: center;
-                    padding: 20px 0;
-                    font-size: $font-little + 1;
-                    text-decoration: none;
+                    justify-content: center;
+                    border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+                    text-align: center;
+                    font-weight: 500;
+                    color: #3d215c;
+                    @include px2rem('height', 80);
 
                     span {
-                        display: inline-flex;
-                        align-items: center;
-                        color: #5b5b5b;
+                        display: inline-block;
+                        margin-right: 10px;
+                        @include px2rem('width', 15);
+                        @include px2rem('height', 15);
+                        @include background-image();
+                        background-image: url('../assets/images/sign_out_icon.png');
+                    }
+                }
 
-                        &.check-option__language::before {
-                            content: '';
-                            display: inline-block;
-                            margin-right: 10px;
-                            @include px2rem('width', 30);
-                            @include px2rem('height', 30);
-                            @include background-image();
-                            background-image: url('../assets/images/icon_language.png');
-                        }
+                .dialog-footer {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: center;
+                    align-items: center;
 
-                        &.check-option__game::before {
-                            content: '';
-                            display: inline-block;
-                            margin-right: 10px;
-                            @include px2rem('width', 30);
-                            @include px2rem('height', 30);
-                            @include background-image();
-                            background-image: url('../assets/images/icon_game_des.png');
-                        }
+                    span {
+                        width: 50%;
+                        padding: 14px 0;
+                        text-align: center;
+                        color: #a520c5;
 
-                        &.check-option__help::before {
-                            content: '';
-                            display: inline-block;
-                            margin-right: 10px;
-                            @include px2rem('width', 30);
-                            @include px2rem('height', 30);
-                            @include background-image();
-                            background-image: url('../assets/images/icon_helper.png');
-                        }
-
-                        &.check-option__contact::before {
-                            content: '';
-                            display: inline-block;
-                            margin-right: 10px;
-                            @include px2rem('width', 30);
-                            @include px2rem('height', 30);
-                            @include background-image();
-                            background-image: url('../assets/images/icon_commite_us.png');
-                        }
-
-                        &.check-option__out::before {
-                            content: '';
-                            display: inline-block;
-                            margin-right: 10px;
-                            @include px2rem('width', 30);
-                            @include px2rem('height', 30);
-                            @include background-image();
-                            background-image: url('../assets/images/icon_sign_out.png');
-                        }
-
-                        &.check-arrow {
-                            @include px2rem('width', 11);
-                            @include px2rem('height', 11);
-                            background-image: url('../assets/images/icon_push_right.png');
-                            @include background-image()
-                        }
-
-                        &.lang-tip {
-                            position: absolute;
-                            right: 30px;
-                            font-size: $font-little-s;
-                            color: #c4c2c2;
+                        &:first-child {
+                            color: #636875;
+                            border-right: 1px solid rgba(0, 0, 0, 0.08);
                         }
                     }
                 }
