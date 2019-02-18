d00ad57c20b8a35ede7164d072203f5da1381249
kevin <kenvin.zheng@drigle.com>
2019-02-18 15:18:24 +0800

    update login && join

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,5 @@
 export default {
-    //apiDomain: 'http://47.75.115.65:5082/api/'
-    apiDomain: 'http://onecoin2.inex.exchange/api/'
+    // apiDomain: 'http://47.75.115.65:5082/api/'
+    // apiDomain: 'http://onecoin2.inex.exchange/api/'
+    apiDomain: 'http://testapi.luckyinc.org/api/'
 }
